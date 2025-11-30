c0c5f68e05d62d73b601232bfd01c4c2527685e9
Steffan Andrews <stef@orchetect.com>
2025-11-30 14:07:28 -0800

    Updated unit tests

--- Tests/SwiftPListTests/AnyPList/AnyPList Tests.swift
+++ Tests/SwiftPListTests/AnyPList/AnyPList Tests.swift
@@ -9,20 +9,20 @@ import XCTest
 
 final class AnyPList_Tests: XCTestCase {
     func testInit_XML_DictionaryRoot() throws {
-        let anyPL = try AnyPList(xml: kSamplePList.DictRootAllValues.XML.raw)
-        guard case let .dictionaryRoot(pl) = anyPL.plist else { XCTFail(); return }
-        kSamplePList.DictRootAllValues.verify(matches: pl)
+        let anyPList = try AnyPList(xml: kSamplePList.DictRootAllValues.XML.raw)
+        guard case let .dictionaryRoot(plist) = anyPList else { XCTFail(); return }
+        kSamplePList.DictRootAllValues.verify(matches: plist)
     }
     
     func testInit_XML_ArrayRoot() throws {
-        let anyPL = try AnyPList(xml: kSamplePList.ArrayRootBasicValues.XML.raw)
-        guard case let .arrayRoot(pl) = anyPL.plist else { XCTFail(); return }
-        kSamplePList.ArrayRootBasicValues.verify(matches: pl)
+        let anyPList = try AnyPList(xml: kSamplePList.ArrayRootBasicValues.XML.raw)
+        guard case let .arrayRoot(plist) = anyPList else { XCTFail(); return }
+        kSamplePList.ArrayRootBasicValues.verify(matches: plist)
     }
     
     func testInit_XML_StringRoot() throws {
-        let anyPL = try AnyPList(xml: kSamplePList.StringRoot.XML.raw)
-        guard case let .stringRoot(pl) = anyPL.plist else { XCTFail(); return }
-        kSamplePList.StringRoot.verify(matches: pl)
+        let anyPList = try AnyPList(xml: kSamplePList.StringRoot.XML.raw)
+        guard case let .stringRoot(plist) = anyPList else { XCTFail(); return }
+        kSamplePList.StringRoot.verify(matches: plist)
     }
 }
